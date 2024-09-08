ceebaa7fb6830419a8df1dc01387ef00564f19f3
UosefHamza <uosefhamza0@gmail.com>
2024-09-08 16:42:06 +0300

    Part 03 Parameters with Routing

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace MVCSession1
 {
     public class Program
@@ -15,11 +17,27 @@ namespace MVCSession1
                     await context.Response.WriteAsync("You Are at Home Page");
                 });
 
-                endpoints.MapGet("/Products", async context =>
-                {
-                    await context.Response.WriteAsync("You Are at Products Page ");
-                }); 
 
+                endpoints.MapGet("/Products/{ID:int?}", async context =>
+                {
+                    var idData = context.Request.RouteValues["ID"];
+                    if (idData is not null)
+                    {
+                        int data = Convert.ToInt32(idData);
+                        await context.Response.WriteAsync($"You Are at Products Page id => {data} ");
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync($"You Are at Products Page");
+                    }
+                });
+
+                endpoints.MapGet("/Books/{ID}/{Auther:alpha:minlength(5):maxlength(10)}", async context =>
+                {
+                    int id = Convert.ToInt32(context.Request.RouteValues["ID"]);
+                    string name = context.Request.RouteValues["Auther"].ToString() ;
+                    await context.Response.WriteAsync($"You Are at Books Page id => {id} , At Author => {name} ");
+                });
             });
 
             app.Run(async(HttpContext) => { await HttpContext.Response.WriteAsync("Your Requested Page Not Found"); } );
